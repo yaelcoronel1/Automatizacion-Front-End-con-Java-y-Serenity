779afd489a151d17829f70a1f87a10ed62ebecef
Yael Coronel <rodriguezcoronelyael@gmail.com>
2025-11-21 16:32:40 -0600

    Delete src directory

--- src/main/java/co/com/practica/yaelcoronel/userinterfaces/AbrirUrlNavegador.java
+++
@@ -1,9 +0,0 @@
-package co.com.practica.yaelcoronel.userinterfaces;
-
-import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl("https://www.demoblaze.com/")
-
-public class AbrirUrlNavegador extends PageObject {
-}
